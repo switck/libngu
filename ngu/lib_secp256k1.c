dfb3cf51b588908b4966f32059143acc97e68d0a
scgbckbone <scgbckbone@proton.me>
2023-03-15 19:01:08 +0100

    bump to v0.3.0

--- ngu/lib_secp256k1.c
+++ ngu/lib_secp256k1.c
@@ -1,12 +1,12 @@
 #ifndef NO_QSTR
 
-# ifndef HAVE_CONFIG_H
-#  define HAVE_CONFIG_H
-# endif
-
 # define USE_EXTERNAL_DEFAULT_CALLBACKS
 
 # include "src/secp256k1.c"
+# include "src/modules/extrakeys/main_impl.h"
+# include "src/modules/schnorrsig/main_impl.h"
+# include "src/modules/recovery/main_impl.h"
+# include "src/modules/ecdh/main_impl.h"
 # include "src/precomputed_ecmult.c"
 # include "src/precomputed_ecmult_gen.c"
 #endif
